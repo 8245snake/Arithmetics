02aaa39c581ac88af81892965fdd4ef3ab0aaf66
Hanyu <8245snake@gmail.com>
2021-11-14 17:41:29 +0900

    プロジェクト ファイルを追加します。

---
+++ Arithmetics/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Management.Automation;
+
+namespace Arithmetics
+{
+    public class Calculator : IDisposable
+    {
+        private RunspaceInvoke _invoker;
+
+        /// <summary>
+        /// デフォルトコンストラクタ
+        /// </summary>
+        public Calculator()
+        {
+            _invoker = new RunspaceInvoke();
+        }
+
+        /// <summary>
+        /// 文字列の計算式を計算した結果を返す
+        /// </summary>
+        /// <param name="expression">計算式</param>
+        /// <returns>計算結果</returns>
+        public double Calculate(string expression)
+        {
+            string converted = Converter.Convert(expression);
+            var result = _invoker.Invoke(converted)?.FirstOrDefault()?.BaseObject.ToString();
+            return double.Parse(result);
+        }
+
+        public void Dispose()
+        {
+            _invoker.Dispose();
+            _invoker = null;
+        }
+    }
+}
---
+++ Arithmetics/Converter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Arithmetics
+{
+    internal static class Converter
+    {
+
+        private static readonly Tuple<string, string>[] MathFunctions =
+        {
+            new Tuple<string, string>("abs(", "[Math]::Abs("),
+            new Tuple<string, string>("atan2(", "[Math]::Atan2("),
+            new Tuple<string, string>("bigmul(", "[Math]::BigMul("),
+            new Tuple<string, string>("ceiling(", "[Math]::Ceiling("),
+            new Tuple<string, string>("cos(", "[Math]::Cos("),
+            new Tuple<string, string>("cosh(", "[Math]::Cosh("),
+            new Tuple<string, string>("divrem(", "[Math]::DivRem("),
+            new Tuple<string, string>("exp(", "[Math]::Exp("),
+            new Tuple<string, string>("floor(", "[Math]::Floor("),
+            new Tuple<string, string>("ieeeremainder(", "[Math]::IEEERemainder("),
+            new Tuple<string, string>("log(", "[Math]::Log("),
+            new Tuple<string, string>("log10(", "[Math]::Log10("),
+            new Tuple<string, string>("max(", "[Math]::Max("),
+            new Tuple<string, string>("min(", "[Math]::Min("),
+            new Tuple<string, string>("pow(", "[Math]::Pow("),
+            new Tuple<string, string>("round(", "[Math]::Round("),
+            new Tuple<string, string>("sign(", "[Math]::Sign("),
+            new Tuple<string, string>("sin(", "[Math]::Sin("),
+            new Tuple<string, string>("sinh(", "[Math]::Sinh("),
+            new Tuple<string, string>("sqrt(", "[Math]::Sqrt("),
+            new Tuple<string, string>("tan(", "[Math]::Tan("),
+            new Tuple<string, string>("tanh(", "[Math]::Tanh("),
+            new Tuple<string, string>("truncate(", "[Math]::Truncate("),
+        };
+
+        public static string Convert(string expression)
+        {
+            string converted = expression.ToLower().Replace(" ", "");
+
+            foreach (Tuple<string, string> tuple in MathFunctions)
+            {
+                converted = converted.Replace(tuple.Item1, tuple.Item2);
+            }
+
+            // ２重で変換してしまったとき用
+            converted = converted.Replace("a[Math]::Cos(", "[Math]::Acos");
+            converted = converted.Replace("a[Math]::Sin(", "[Math]::Asin");
+            converted = converted.Replace("a[Math]::Tan(", "[Math]::Atan");
+            converted = converted.Replace("[math]::[Math]::", "[Math]::");
+
+            return converted;
+        }
+
+    }
+}
